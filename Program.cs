6ceefe0c25057c4eb2649cb29afe5427ab01d519
nijarquin-n <nijarquin@uamv.edu.ni>
2026-05-08 13:46:48 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,43 +1,12 @@
-﻿//
+﻿double[] V = { 3, 4, 5, 6, 7 };
 
-using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
+double suma = 0;
 
-int[] notas = new int[15];
-int suma = 0;
-double promedio = 0;
-
-for (int i = 0; i < notas.Length; i++)
+for (int i = 0; i < V.Length; i++)
 {
-    try
-    {
-        
-        Console.WriteLine($"ingrese la nota del estudiante(1 + 1): ");
-        int nota = int.Parse(Console.ReadLine());
+    suma += V[i] * V[i];
+}
 
-        if (nota < 0 || nota > 100)
-        {
-            throw new ArgumentOutOfRangeException("La nota debe ser entre 0 y 100.");
-        }
-        
-        notas[i] = nota;
-        suma += nota;
+double magnitud = Math.Sqrt(suma);
 
-    {
-    catch (FormatException)
-    }
-       Console.WriteLine("Entrada no valida.por favor, ingree un numero entero.");
-       i--;// Decrementar para volver a pedir la nota del mismo estudiante
-    }
-    catch (ArgumentOutOfRangeException ex)
-    {
-         Console.WriteLine(Exception.Messsagen);
-         i--; // Decrementar para volver a pedir la nota del mismo estudiante 
-    }
-    Catch (Exception ex)
-    {
-        Console.WriteLine($"ocurrio un error : {ex.Message}");
-        i--; // Decrementar para volver a pedir la nota del mismo estudiante
-    }
-    
-}   
+Console.WriteLine("La magnitud del vector es: " + magnitud);
