07dc5b115deac66db48d312bbbf8201edc289c63
nijarquin-n <nijarquin@uamv.edu.ni>
2026-05-08 13:48:40 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,50 @@
-﻿int[] A = { 15, 18, 20, 25, 30 };
-int[] B = { 5, 8, 10, 12, 15 };
-int[] R = new int[5];
+﻿using System.Numerics;
 
-for (int i = 0; i < 5; i++)
+int[] vector1 = new int [5];
+int [] vector2 = new int [5];
+int[] vectorSuma = new int [5];
+Console.WriteLine("por favor ingrese los valores del primer vector");
+for (int i = 0; i < vector1 .Length; i++)
 {
-    R[i] = A[i] - B[i];
+    try
+    {
+        Console.WriteLine($"valor{i + 1}: ");
+        vector1[i] = int.Parse(Console.ReadLine());
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error : entrada no valida, por favor ingrese un mumero");
+        i--; // Decrementar el indice para volver a solicitar el valor
+        Console.ResetColor();
+    }
 }
-
-Console.WriteLine("Vector resultante:");
-
-for (int i = 0; i < 5; i++)
+ Console.WriteLine("por favor ingrese los valores del degundo vector");
+ for (int i = 0; i < vector2.Length; i++)
 {
-    Console.Write(R[i] + " ");
+    try
+    {
+        Console.WriteLine($"valor{i + 1}:");
+        vector2[i] = int.Parse(Console.ReadLine());
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error : entrada no valida, por favor ingrese un mumero");
+        i--; // Decrementar el indice para volver a solicitar el valor
+        Console.ResetColor();
+    }
 }
+     // Suma los vectores
+    for ( int i = 0; i < vectorSuma.Length; i++)
+   {
+    vectorSuma[i] = vector1[i] + vector2[i];
+   }
+
+   //Muestra el arreglo resultante
+   Console.WriteLine("suma de vectores: ");
+   for (int i = 0; i < vectorSuma.Length; i++)
+  {
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine($"valor {i + i}: {vectorSuma[i]}");1
+  }
